f1d2178ad85b563762be91074cfbdc9fb3e31a55
Wong Jia Hau <hou32hou@gmail.com>
2021-06-26 16:20:01 +0800

    feat(mysql): a proof of concept for retrieving SQL query types from MySQL

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,41 @@
-mod ast;
-mod generate;
-mod tests;
+// mod ast;
+// mod generate;
+// mod tests;
 
-mod typecheck;
+// mod typecheck;
+use mysql::prelude::*;
 
-fn main() {}
+fn main() {
+    let username = "root";
+    let password = "";
+    let hostname = "localhost";
+    let database_name = "nation";
+    let url = format!(
+        "mysql://{}:{}@{}/{}",
+        username, password, hostname, database_name
+    );
+
+    let pool = mysql::Pool::new(url).unwrap();
+
+    let mut conn = pool.get_conn().unwrap();
+
+    let query = "
+        select 
+            continents.name, count(*), regions.name
+        from 
+            continents inner join regions 
+                on continents.continent_id = regions.continent_id 
+        where regions.name = ?
+        group by continents.name
+        ";
+    let stmt = conn.prep(query).unwrap();
+    let columns = stmt
+        .columns()
+        .iter()
+        .map(|column| (column.name_str().to_string(), column.column_type()))
+        .collect::<Vec<(String, mysql::consts::ColumnType)>>();
+
+    println!("query = {}", query);
+    println!("columns = {:#?}", columns);
+    println!("number of parameters = {:#?}", stmt.num_params());
+}
